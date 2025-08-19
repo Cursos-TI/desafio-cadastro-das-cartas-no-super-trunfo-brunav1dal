87de3e5558f55266002421664c79a13bd465a258
brunav1dal <brunavidal2209@gmail.com>
2025-08-19 23:24:59 +0000

    Coleta de dados e registro de cartas do jogo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,42 +1,45 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <locale.h>
+#include <stdlib.h>   // biblioteca padrão
+#include <stdio.h>    // entrada e saída (printf, scanf)
+#include <locale.h>   // permite usar acentuação
 
 int main()
 {
-    setlocale(LC_ALL,"Portuguese");
-    char estado;
-    int popul;
-    char codigo[10];
-    char cidade[50];
-    int area;
-    int p_turis;
-    float PIB;
+    setlocale(LC_ALL,"Portuguese"); // define idiomaa
 
+    // variáveis para armazenar dados da carta(declarações de variáveis)
+    char estado;        // guarda 1 caractere 
+    int popul;         // população (número inteiro)
+    char codigo[10];    // código da carta (texto)
+    char cidade[50];   // nome da cidade (texto)
+    int area;          // área em km²
+    int p_turis;       // pontos turísticos
+    float PIB;         // produto interno bruto (número decimal)
+
+    //CARTA A 
     printf("\nInforme os dados da CARTA A \n");
 
     printf("Estado: ");
-    scanf(" %c",&estado);
+    scanf(" %c",&estado);   // lê 1 caractere 
 
     printf("Código da carta:");
-    scanf("%s",codigo);
+    scanf("%s",codigo);     // lê array 
 
     printf("Nome da cidade: ");
-    scanf("%s",cidade);
+    scanf("%s",cidade);     // lê array
 
     printf("População: ");
-    scanf("%d",&popul);
+    scanf("%d",&popul);     // lê número inteiro
 
     printf("Área: ");
-    scanf("%d",&area);
+    scanf("%d",&area);      // lê número inteiro
 
     printf("PIB: ");
-    scanf("%f",&PIB);
+    scanf("%f",&PIB);       // lê número decimal
 
     printf("Pontos Turísticos: ");
-    scanf("%d",&p_turis);
+    scanf("%d",&p_turis);   // lê número inteiro
 
-
+    // mostra os dados lidos da carta A
     printf("\nDados da Carta A\n");
     printf("Estado: %c\n", estado);
     printf("Código: %s\n", codigo);
@@ -46,7 +49,7 @@ int main()
     printf("PIB: %f\n", PIB);
     printf("Pontos Turísticos: %d\n", p_turis);
 
-
+    // CARTA B
     printf("\nInforme os dados da CARTA 2 \n");
 
     printf("Estado: ");
@@ -70,6 +73,8 @@ int main()
     printf("Pontos Turísticos: ");
     scanf("%d",&p_turis);
 
+    
+// mostra os dados lidos da carta B
     printf("\nDados da Carta BA\n");
     printf("Estado: %c\n", estado);
     printf("Código: %s\n", codigo);
@@ -79,7 +84,5 @@ int main()
     printf("PIB: %f\n", PIB);
     printf("Pontos Turísticos: %d\n", p_turis);
 
-
-    return 0;
-
+    return 0; // finaliza o programa
 }
